8e0e0215289901e8ab90eaad54d048332c8e8fa9
flycanflyxd <flycanflyxd@gmail.com>
2015-07-20 09:47:46 +0800

    Complete triangle

--- Project/capture.h
+++ Project/capture.h
@@ -6,7 +6,14 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <IL/il.h>
 #include <IL/ilu.h>
+#include <vector>
 
-void capture(const glm::mat4 &MVP, const GLfloat g_vertex_buffer_data[], const GLfloat g_color_buffer_data[]);
+class triangle
+{
+public:
+	glm::vec4 vertices[3];
+};
+
+void capture(const glm::mat4 &MVP, const GLfloat g_vertex_buffer_data[], const int g_vertex_buffer_data_size, const GLfloat g_color_buffer_data[]);
 
 #endif
